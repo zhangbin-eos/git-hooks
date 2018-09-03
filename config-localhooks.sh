690c7f239e9d05f6e335812e358d71219ab4da8d
zhangbin_eos <zhangbin.eos@foxmail.com>
2018-09-03 21:16:13 +0800

    feat(config):config hooks by 'ln -s'

--- config-localhooks.sh
+++ config-localhooks.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
+set -x
+
+hooks="commit-msg pre-commit"
+
 
 if ! [ -d ".git/hooks/"  ]
 then
-        cur=$(pwd)
+        echo "not find git repo"
+        exit 1
 fi
+
+cur=$(pwd)
+cd ".git/hooks/"
+rm -rf $hooks
+
+for i in $hooks
+do
+ln -s ../../$i $i
+done
+
+cd $cur
+
+echo "config ok"
