3ecf99cf083c398191202e42f4209b9959a56c7a
zhangbin-eos <zhangbin.eos@foxmail.com>
2018-09-15 17:25:16 +0800

    feat(config):修改配置脚本,增加对子模块配置的支持

--- config-localhooks.sh
+++ config-localhooks.sh
@@ -5,14 +5,23 @@ hooks="commit-msg pre-commit"
 
 chmod +x -R $srcpath
 
-if ! [ -d ".git/hooks/"  ]
+if [ -d ".git" ]
 then
-        echo "not find git repo"
+        obj_hooks_dir=".git/hooks/"
+elif [ -f ".git" ]
+then
+        obj_hooks_dir="$(cat .git | awk -F': ' '{print $2}')/hooks/"
+else
+        echo "not find  .git "
         exit 1
 fi
 
 cur=$(pwd)
-cd ".git/hooks/"
+if ! cd "$obj_hooks_dir"
+then
+        exit 2
+fi
+
 rm -rf $hooks
 
 for i in $hooks
